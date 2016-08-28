46591ed4dc1cad1a7a6c2ee44c5140f5529b930d
davidreis97 <david.alex.reis.97@gmail.com>
2016-08-28 13:01:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # reddit-reverse-bot
 Reddit bot that reverses a comment when it's preceded by "!reverse"
+
+Built following a guide available on http://pythonforengineers.com/build-a-reddit-bot-part-1/
--- bot.py
+++ bot.py
@@ -1,2 +1,21 @@
-PAneleira
-sad
+import praw
+import pdb
+import re
+import os
+from login-details import * #the login details are on this python file, as strings "USERNAME" and "PASSWORD"
+
+user_agent = ("reverse-text 0.1 // Contact me at /u/SilverTroop")
+
+r = praw.Reddit(user_agent = user_agent)
+
+subreddit = r.get_subreddit("bottesting")
+
+if not  os.path.isfile("config_bot.py"):
+    print "You must create a file named login-details.py with your username and password."
+    exit(1)
+
+r.login(USERNAME, PASSWORD)
+
+if not os.path.isfile("comments_replied_to.txt"):
+    posts_replied_to = []
+ dss

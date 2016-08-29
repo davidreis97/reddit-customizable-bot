99e569d64429fdf0d9867a7245e5b324bad3ac09
davidreis97 <david.alex.reis.97@gmail.com>
2016-08-29 17:01:03 +0100

    First working version
    Changed the login storage and added code to detect "!reverse" and
    reverse the comment. Also added a lazy way to limit the requests to the
    api.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # reddit-reverse-bot
 Reddit bot that reverses a comment when it's preceded by "!reverse"
 
-Built following a guide available on http://pythonforengineers.com/build-a-reddit-bot-part-1/
+Built (kind of) following a guide available on http://pythonforengineers.com/build-a-reddit-bot-part-1/, although this bot and the one on the tutorial have different features and purposes.
+
+Message me at /u/SilverTroop
--- bot.py
+++ bot.py
@@ -2,19 +2,45 @@ import praw
 import pdb
 import re
 import os
-from login-details import * #the login details are on this python file, as strings "USERNAME" and "PASSWORD"
+import time
 
-user_agent = ("reverse-text 0.1 // Contact me at /u/SilverTroop")
+with open("login-details.txt","r") as l:
+    login_details = l.read()
+    login_details = login_details.split("\n")
+
+user_agent = ("reverse-text 0.1 // Contact me at /u/SilverTroop or at https://github.com/davidreis97/")
 
 r = praw.Reddit(user_agent = user_agent)
 
-subreddit = r.get_subreddit("bottesting")
-
-if not  os.path.isfile("config_bot.py"):
-    print "You must create a file named login-details.py with your username and password."
+if not  os.path.isfile("login-details.txt"):
+    print ("You must create a file named login-details.txt with your username and password.")
     exit(1)
 
-r.login(USERNAME, PASSWORD)
+#deprecated
+r.login(login_details[0], login_details[1])
+#deprecated
 
-if not os.path.isfile("comments_replied_to.txt"):
-    posts_replied_to = []
+if not os.path.isfile("comments_answered.txt"):
+    comments_answered = []
+else:
+    with open("comments_answered.txt","r") as f:
+        comments_answered = f.read()
+        comments_answered = comments_answered.split("\n")
+
+while 1:
+    print (comments_answered)
+    submissions = r.get_subreddit('bottesting').get_new(limit=1)
+    for submission in submissions:
+        for comment in submission.comments:
+            if '!reverse' in comment.body:
+                com_sub_id = str(submission.id) + str(comment.id)
+                if com_sub_id not in comments_answered:
+                    response = comment.body.replace("!reverse ","",1)
+                    response = response[::-1]
+                    comment.reply("###reverse-text-bot\n" + response + "\n___________________\n**I am a bot that reverses text. Come find me at [GitHub](https://github.com/davidreis97/reddit-reverse-bot) or talk to my human creator, /u/SilverTroop**\n")
+                    comments_answered.append(com_sub_id)
+                    with open("comments_answered.txt", "w") as w:
+                        for com_id in comments_answered:
+                            w.write(com_id + "\n")
+                    print ("Answered to comment", comment, "in thread", submission)
+    time.sleep(10)
